59ac86f3b4c1d10c51117f7f011862573f5fc0ea
surya-lohan <suryanshlohan@gmail.com>
2025-10-04 00:47:16 +0530

    Some basic stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,39 @@ import axios from 'axios'
 
 
 
-const response = await axios.get(`${import.meta.env.VITE_CRYPTOPRICE_API_URL}/getCryptoList`, {
+const symbolResponse = await axios.get(`${import.meta.env.VITE_CRYPTOPRICE_API_URL}/getData`, {
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_FREECRYPTO_API_KEY}`
+  },
+  params: {
+    currency: 'USD',
+    symbol: 'BTC'
   }
 })
 
+const topSymbolsResponse = await axios.get(`${import.meta.env.VITE_CRYPTOPRICE_API_URL}/getTop`, {
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_FREECRYPTO_API_KEY}`
+  },
+  params: {
+    top: 10,
+  }
+})
 
-const showResponse = (response) => {
-  console.log('Response:', response.data)
-  return JSON.stringify(response.data)
-}
+const data = symbolResponse.data
 
+console.log(data.symbols[0].last)
+console.log(topSymbolsResponse.data)
 
 const App = () => {
   return (
-    <div>{showResponse(response)}</div>
+    <div>
+      <h1>Crypto Price App</h1>
+      <p>{data.symbols[0].symbol}</p>
+      <p>{data.symbols[0].last}</p>
+      <h2>Here are the top 10 coins </h2>
+      {/* <p>{topSymbolsResponse.data}</p> */}
+    </div>
   )
 }
 
